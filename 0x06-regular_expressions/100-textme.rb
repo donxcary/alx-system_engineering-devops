7e298dcde31129057a632f9a792fd84b1a39ef94
donxcary <donxcary@gmail.com>
2024-02-27 14:28:04 +0000

    alx-system_engineering-devops

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+# This code takes a command-line argument and extracts the from, to, and flags fields from it
+import re
+import sys
+input = sys.argv[1]
+pattern = r"\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]"
+matches = re.findall(pattern, input)
+output = ",".join([",".join(match) for match in matches])
+print(output)
+
